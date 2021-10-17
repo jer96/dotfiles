2106b3365833be1998f46e6f8a6a3ced2eba382b
🚀 Jeremiah Bill <jbill8@gatech.edu>
2021-10-16 20:39:14 -0400

    updated vim settings
    reference: https://github.com/VapourNvim/VapourNvim/blob/main/lua/vapour/options/init.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,22 +1,62 @@
-vim.wo.number=true -- always show line numbers
-vim.wo.relativenumber=true -- relative line numbers
-vim.o.hidden=true -- no save when switching buffers
-vim.o.hlsearch=false -- set highlight on search
-vim.o.mouse='a' -- enable mouse mode
-vim.o.ignorecase=true -- case insensitive search unless /C
-vim.o.wrap=false -- turn of word wrap
+vim.cmd('filetype plugin indent on')
 vim.o.completeopt='menuone,noselect' -- set for better completion experience
+vim.o.shortmess = vim.o.shortmess .. 'c'
+vim.o.hidden = true
+vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+vim.o.pumheight = 10
+vim.o.fileencoding = "utf-8"
+vim.o.cmdheight = 2
+vim.o.splitbelow = true
+vim.o.termguicolors = true
+vim.o.splitright = true
+vim.opt.termguicolors = true
+vim.o.conceallevel = 0
+vim.o.showtabline = 2
+vim.o.showmode = false
+vim.o.backup = false
+vim.o.writebackup = false
+-- vim.o.updatetime = 300
+-- vim.o.timeoutlen = 100
+vim.o.clipboard = "unnamedplus"
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.scrolloff = 5
+vim.o.sidescrolloff = 1
+vim.o.mouse = "a"
 
--- indentation
-vim.o.tabstop=2
-vim.o.softtabstop=2
-vim.o.shiftwidth=2
-vim.o.expandtab=true
+vim.wo.wrap = false
+vim.wo.number = true
+vim.wo.relativenumber=true -- relative line numbers
+vim.wo.cursorline = true
+vim.wo.signcolumn = "yes"
+
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.softtabstop = 2
+vim.bo.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.expandtab = true
+vim.bo.expandtab = true
 vim.o.smarttab=true
-vim.o.autoindent=true
 vim.o.smartindent=true
 vim.o.breakindent=true
 vim.o.copyindent=true
 
--- turn on file type plugin
-vim.api.nvim_command('filetype plugin indent on')
+-- Disable various builtin plugins in Vim that bog down speed
+vim.g.loaded_matchparen        = 1
+vim.g.loaded_matchit           = 1
+vim.g.loaded_logiPat           = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_tarPlugin         = 1
+vim.g.loaded_gzip              = 1
+vim.g.loaded_zipPlugin         = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_shada_plugin      = 1
+vim.g.loaded_spellfile_plugin  = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_remote_plugins    = 1
