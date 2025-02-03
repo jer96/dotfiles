fbecf8d6b0ad3712d248c333c82ba22c6f4ee712
jer <jbill7696@gmail.com>
2025-02-02 23:39:33 -0500

    feat: basic diffview keymaps

--- .config/nvim/lua/plugins/diff-view.lua
+++ .config/nvim/lua/plugins/diff-view.lua
@@ -1,3 +1,7 @@
 return {
     "sindrets/diffview.nvim",
+    keys = {
+        { "<leader>df", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+        { "<leader>dx", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    },
 }
